a9ecf2759383e0e43b93ca7815c34bab28301f1c
Cody Soyland <codysoyland@gmail.com>
2010-05-20 00:31:49 -0500

    Convert to new-style tastypie API

--- tests/notes/api/resources.py
+++ tests/notes/api/resources.py
@@ -1,6 +1,10 @@
-from tastypie.resources import Resource
-from notes.api.representations import NoteRepresentation
+from tastypie.resources import ModelResource
+from notes.models import Note
 
-class NoteResource(Resource):
-    representation = NoteRepresentation
-    url_prefix = 'notes'
+class NoteResource(ModelResource):
+    def get_resource_uri(self, bundle):
+        return '/api/notes/%s/' % bundle.obj.id
+
+    class Meta:
+        resource_name = 'notes'
+        queryset = Note.objects.all()
--- tests/urls.py
+++ tests/urls.py
@@ -4,10 +4,14 @@ from notes.api.resources import NoteResource
 import os
 from django.conf import settings
 
-note_resource = NoteResource()
+from notes.api.resources import NoteResource
+from tastypie.api import Api
+
+api = Api(api_name='v1')
+api.register(NoteResource())
 
 urlpatterns = patterns('',
-    (r'^api/notes/', include(note_resource.urls)),
+    (r'^api/', include(api.urls)),
     (r'(?P<path>pieshop\.js)', 'django.views.static.serve', {
         'document_root': os.path.join(settings.BASE_PATH, '../src/'),
     }),
